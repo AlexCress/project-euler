c094010a129d9d1bb1e45e886d175412e232e2e2
AlexCress <alex_cress@hotmail.com>
2016-04-10 10:26:07 -0600

    Revert "Attempting to switch ArrayList to Array"
    This reverts commit c31a524949c98b42f5bf9c618d01e86e7b71f918.

--- src/projectEuler/ProjectEuler023.java
+++ src/projectEuler/ProjectEuler023.java
@@ -4,103 +4,72 @@ import java.util.ArrayList;
 
 public class ProjectEuler023 {
 	
-	public static final int MIN_ABUNDANT = 12;
-	public static final int MAX_ABUNDANT = 28123;
-	private ArrayList<Integer> abundantNumbers;
+	ArrayList<Integer> abundantNumbers;
 	
 	public int solve(){
 		
-		abundantNumbers = new ArrayList<Integer>(6965); //6965 taken after solution was confirmed, optimization
+		int nonabundantSum = 0;
+		abundantNumbers = new ArrayList<Integer>();
 		
 		//12 is given in challenge as first abundant number
-		//28123 is given as the upper bound
-		long start = System.currentTimeMillis();
-		for(int i = MIN_ABUNDANT; i <= MAX_ABUNDANT; i++){
+		//28123 is given as the upper bound, could technically be 28123-12
+		for(int i = 12; i <= 28123; i++){
 			if(isAbundant(i)){
 				abundantNumbers.add(i);
 			}
 		}
-		long end = System.currentTimeMillis();
 		
-		System.out.println("Time for initialization: " + (end - start));
-		
-		start = System.currentTimeMillis();
-		int nonabundantSum = 0;
-		for(int i = 1; i <= MAX_ABUNDANT; i++){
+		for(int i = 1; i <= 28123; i++){
 			if( ! (isSumOfAbundantNumbers(i))){
 				nonabundantSum += i;
 			}
 		}
-		end = System.currentTimeMillis();
 		
-		System.out.println("Time for solving: " + (end - start));
-				
+		
 		return nonabundantSum;
 	}
 	
 	private boolean isAbundant(final int number){
 
-		return(sumOfDivisors(number) > number * 2);
+		int i = getSumOfProperDivisors(number);
+		if(i > number){
+			return true;
+		}
+		
+		return false;
 	}
 	
-	private int sumOfDivisors(int number){
-
-		int sum = 1;
-		for(int i = 2; i * i <= number; i++){
-			int p = 1;
-			while(number % i == 0){
-				p = p * i + 1;
-				number /= i;
-			}
-			sum *= p;
-		}
-		if(number > 1){
-			sum *= 1 + number;
-		}
-		return sum;
+	private int getSumOfProperDivisors(final int number){
+		int sum = 0;
 		
-//		int sum = 0;
-//		
-//		for(int i = 1; i <= number / 2; i++){
-//			if(number % i == 0){
-//				sum += i;
-//			}
-//		}
-//		
-//		return sum;
+		for(int i = 1; i <= number / 2; i++){
+			if(number % i == 0){
+				sum += i;
+			}
+		}
+		
+		return sum;
+		//aa
 	}
 	
 	private boolean isSumOfAbundantNumbers(final int number){
-		
 		for(int i = 0; i < abundantNumbers.size(); i++){
-			if(number - abundantNumbers.get(i) < MIN_ABUNDANT){
-				return false;
-			}
-			int n = number - abundantNumbers.get(i);
-			if(abundantNumbers.contains(n)){
-				return true;
+			for(int j = i; j < abundantNumbers.size(); j++){
+				int sum = abundantNumbers.get(i) + abundantNumbers.get(j);
+				
+				//All possibilities have been exhausted
+				if(j == i + 1 && sum > number){
+					return false;
+				}
+				//Not necessarily done, reset j to save time
+				else if(sum > number){
+					break;
+				}
+				else if(sum == number){
+					return true;
+				}
 			}
 		}
 		return false;
-		
-		
-//		for(int i = 0; i < abundantNumbers.size(); i++){
-//			for(int j = i; j < abundantNumbers.size(); j++){
-//				int sum = abundantNumbers.get(i) + abundantNumbers.get(j);
-//				
-//				//All possibilities have been exhausted
-//				if(j == i + 1 && sum > number){
-//					return false;
-//				}
-//				//Not necessarily done, reset j to save time
-//				else if(sum > number){
-//					break;
-//				}
-//				else if(sum == number){
-//					return true;
-//				}
-//			}
-//		}
-//		return false;
 	}
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -2,16 +2,27 @@ package test;
 
 import projectEuler.*;
 
-public class Test{
-    
-    public static void main(String[] args){
-    	long start = System.currentTimeMillis();
-    	
-    	ProjectEuler023 pe = new ProjectEuler023();
-    	System.out.println(pe.solve());
-    	
-    	long end = System.currentTimeMillis();
-    	
-    	System.out.println("Total time: " + (end - start));
-    }
+/**
+ * 
+ * Contains main method to test challenges.
+ * 
+ * 
+ * 
+ * @author Alex
+ * 
+ */
+public class Test {
+
+	public static void main(String[] args){
+
+		long start = System.currentTimeMillis();
+		
+		ProjectEuler023 pe = new ProjectEuler023();
+
+		System.out.println(pe.solve());
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Running time: " + (end - start));
+	}
 }
